b761056b80a9076c443fdc9bd038712348b17e0e
Hyeji Ko <hyko@c10r6s6.42seoul.kr>
2022-07-05 22:04:54 +0900

    2022-07-05

---
+++ philo.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 21:38:13 by hyko              #+#    #+#             */
+/*   Updated: 2022/07/05 22:00:21 by hyko             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	long long	i;
+	long long	sign;
+	long long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((9 <= str[i] && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		if (result * sign > 2147483647)
+			return (-1);
+		else if (result * sign < -2147483648)
+			return (0);
+		i++;
+	}
+	return (result * sign);
+}
+
+int	main(int argc, char **argv)
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
+
+	if (argc < 5 || argc > 6)
+		return (0);
+	number_of_philosophers = ft_atoi(argv[1]);
+	time_to_die = ft_atoi(argv[2]);
+	time_to_eat = ft_atoi(argv[3]);
+	time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
+	else
+		number_of_times_each_philosopher_must_eat = 0;
+}

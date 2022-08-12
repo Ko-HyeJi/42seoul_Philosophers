feca7debd6306d51faca93876c8abaf98b1ac914
Hyeji Ko <hyko@c1r2s5.42seoul.kr>
2022-08-12 17:01:19 +0900

    2022-08-12

--- src/routine.c
+++ src/routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyko <hyko@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: hyko <hyko@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 17:11:54 by hyko              #+#    #+#             */
-/*   Updated: 2022/08/09 18:11:08 by hyko             ###   ########.fr       */
+/*   Updated: 2022/08/12 16:50:07 by hyko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,20 @@
 int	philo_grab_fork(t_philo *philo)
 {
 	pthread_mutex_lock(philo->left_fork); //left_fork
-	// if (death_check(philo) == TRUE)
-	// 	return (-1);
-	print_msg(philo, 'f');
+	if (philo->game->death_flag == 0)
+		print_msg(philo, 'f');
 	pthread_mutex_lock(philo->right_fork); //right_fork
-	// if (death_check(philo) == TRUE)
-	// 	return (-1);
-	print_msg(philo, 'f');
+	if (philo->game->death_flag == 0)
+		print_msg(philo, 'f');
 	return (0);
 }
 
 int	philo_eat(t_philo *philo)
 {
-	print_msg(philo, 'e');
+	if (philo->game->death_flag == 0)
+		print_msg(philo, 'e');
 	philo->last_eat = get_ms_time();
 	philo->eat_cnt++;
-	// if (death_check(philo) == TRUE)
-	// 	return (-1);
 	philo_alarm(philo->game->time_to_eat);
 	// philo->last_eat = get_ms_time();
 	pthread_mutex_unlock(philo->left_fork);
@@ -41,12 +38,10 @@ int	philo_eat(t_philo *philo)
 
 int	philo_sleep(t_philo *philo)
 {
-	// if (death_check(philo) == TRUE)
-	// 	return (-1);	
-	print_msg(philo, 's');
+	if (philo->game->death_flag == 0)
+		print_msg(philo, 's');
 	philo_alarm(philo->game->time_to_sleep);
-	// if (death_check(philo) == TRUE)
-	// 	return (-1);
-	print_msg(philo, 't');
+	if (philo->game->death_flag == 0)
+		print_msg(philo, 't');
 	return (0);
 }
